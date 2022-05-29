5a214ffa570d9f0323f6b2c7e647088dd398bab1
Lana <sitdownrightnow2552@gmail.com>
2022-05-29 22:49:06 +0700

    feat: add menu

--- app/src/main/java/com/example/bkapsm/MainActivity.java
+++ app/src/main/java/com/example/bkapsm/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.bkapsm;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends NavigableActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/bkapsm/NavigableActivity.java
@@ -0,0 +1,44 @@
+package com.example.bkapsm;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+
+public class NavigableActivity extends AppCompatActivity {
+    protected void navigate(Context context, Class<?> target) {
+        if (getClass().equals(target)) return;
+        startActivity(new Intent(context, target));
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.menuitem_create) {
+            return true;
+        }
+
+        if (id == R.id.menuitem_home) {
+            navigate(this, MainActivity.class);
+            return true;
+        }
+
+        if (id == R.id.menuitem_search) {
+            return true;
+        }
+
+        return false;
+    }
+}
